2dbca7999004cefca94a68863c70f68cf2847a00
whitigr25 <whitigr25@gmail.com>
2025-03-23 14:50:17 -0400

    Variables and Data
    Updated code with "Mad Libs" game

--- variables_and_data_types.py
+++ variables_and_data_types.py
@@ -3,3 +3,8 @@ age="25"
 months="2"
 print(name, " is ", age)
 print(f"My name is {name} and I am {age} years old and {months} months old.")
+
+name=input("Enter a name: ")
+print(f"My name is {name} and I am {age} years old and {months} months old.")
+age=input ("Enter an age: ")
+print(f"My name is {name} and I am {age} years old and {months} months old.")
